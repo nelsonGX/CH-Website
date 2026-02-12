2560c5aac46bef0c6f064a2e53b65ca36c17165a
nelsonGX <hi@nelsongx.com>
2026-02-13 04:37:05 +0800

    add banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,19 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "CH's Website",
   description: "Very nice!",
+  openGraph: {
+    title: "CH's Website",
+    description: "Hi, nice to meet you, this is CH's Website. Click to view more!",
+    images: [
+      {
+        url: "https://ch-website.pages.dev/assets/banner.webp",
+        width: 2500,
+        height: 1000,
+        alt: "CH's Website Banner",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
